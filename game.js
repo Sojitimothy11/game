2351edb4e841cbd969662627316ba83a9b99562a
Sojitimothy11 <timothysoji1@gmail.com>
2023-09-15 16:15:14 +0100

    paper, rock and scissors

--- game.js
+++ game.js
@@ -1,96 +1,90 @@
 const prompt = require("prompt-sync")();
 
-let firstInput= parseInt(prompt("insert your first number"));
-let secondInput= parseInt(prompt("insert your second number"));
+function gtComputerChoice(){
+  let choices=["rock","paper","scissors"];
+  let choice= Math.floor(Math.random()*choices.length);
+  console.log("computer picked" +" "+ choices[choice]);
+  return choices[choice];
+}
 
-let addition=firstInput + secondInput;
-console.log(addition);
+function userChoice(){
+  let userInput=prompt(
+    "Rock, Paper or Scissors, Choose One?:"
+  );
 
-// function computerChoice(){
-//   let choices=["red","blue","yellow"];
-//   let choice= Math.floor(Math.random()*choices.length);
-//   console.log("computer picked" +" "+ choices[choice]);
-//   return choices[choice];
-// }
+  if(userInput=="rock"){
+    console.log("You Picked Rock");
+    return "Rock";
+  }else if(userInput=="Paper"){
+    console.log("You Picked Paper");
+    return "Paper";
+  }else if(userInput=="Scissors"){
+    console.log("You Picked Scissors");
+    return "Scissors";
+  }else{
+    console.log("Invalid Input")
+  }
+}
 
-// function userChoice(){
-//   let userInput=prompt(
-//     "what color would you like to play with?:"
-//   ).toLowerCase();
+function game(){
+  let playerSelection= userChoice();
+  let computerSelection=computerChoice();
 
-//   if(userInput=="red"){
-//     console.log("You Picked Red");
-//     return "red";
-//   }else if(userInput=="blue"){
-//     console.log("You Picked Blue");
-//     return "blue";
-//   }else if(userInput=="yellow"){
-//     console.log("You Picked Yellow");
-//     return "yellow";
-//   }else{
-//     console.log("Invalid Input")
-//   }
-// }
+  if(playerSelection===computerSelection){
+    console.log("Its a Tie try Again!!!");
+  }else if(playerSelection==="rock"){
+    if(computerSelection==="paper"){
+      console.log("You lose!, Paper beat Rock");
+      return "lose";
+    }else if(computerSelection==="scissors"){
+      console.log("You win!");
+      return "win";
+    }else{
+      console.log("Its a Tie!");
+      return "tie";
+    }
+  }else if(playerSelection==="paper"){
+    if(computerSelection==="rock"){
+      console.log("You win!, Paper beat rock");
+      return "win";
+    }else if(computerSelection==="paper"){
+      console.log("Its Green,Its a Tie!");
+      return "tie";
+    }else{
+      console.log(" Computer Won!!")
+      return "lose";
+    }
+  }else{
+    console.log("Enter a Valid Color of Red, Blue or Yellow");
+  }
+}
 
-// function game(){
-//   let userColor= userChoice();
-//   let compColor=computerChoice();
+function play(){
+  let round= 0;
+  let userScore= 0;
+  let compScore= 0;
 
-//   if(userColor===compColor){
-//     console.log("Its a Tie try Again!!!");
-//   }else if(userColor==="red"){
-//     if(compColor==="blue"){
-//       console.log("Its Purple,Computer Won!");
-//       return "lose";
-//     }else if(compColor==="yellow"){
-//       console.log("Its Orange,You Won!");
-//       return "won";
-//     }else{
-//       console.log("Its a Tie!");
-//       return "tie";
-//     }
-//   }else if(userColor==="yellow"){
-//     if(compColor==="red"){
-//       console.log("Its Orange,You Won!");
-//       return "won";
-//     }else if(compColor==="blue"){
-//       console.log("Its Green,Its a Tie!");
-//       return "tie";
-//     }else{
-//       console.log("Its Purple, Computer Won!!")
-//       return "lose";
-//     }
-//   }else{
-//     console.log("Enter a Valid Color of Red, Blue or Yellow");
-//   }
-// }
+  for(let i=1; i<=4; i++){
+    let games= game();
+    if(games==="won"){
+      userScore++;
+      round++;
+      console.log(
+        `Round:${round}, userScore ${userScore} :${compScore} compScore`
+      );
+    }else if(games ==="lose"){
+      compScore++;
+      round++;
+      console.log(
+        `Round: ${round}, userScore ${userScore} : ${compScore}compScore`
+      );
+    }else{
+      round++;
+      console.log(
+        `Round:${round}, userScore ${userScore}: ${compScore} compScore`
+      );
+    }
+  }
+}
 
-// function play(){
-//   let round= 0;
-//   let userScore= 0;
-//   let compScore= 0;
-
-//   for(let i=1; i<=4; i++){
-//     let games= game();
-//     if(games==="won"){
-//       userScore++;
-//       round++;
-//       console.log(
-//         `Round:${round}, userScore ${userScore} :${compScore} compScore`
-//       );
-//     }else if(games ==="lose"){
-//       compScore++;
-//       round++;
-//       console.log(
-//         `Round: ${round}, userScore ${userScore} : ${compScore}compScore`
-//       );
-//     }else{
-//       round++;
-//       console.log(
-//         `Round:${round}, userScore ${userScore}: ${compScore} compScore`
-//       );
-//     }
-//   }
-// }
-
-// play();
+play();
